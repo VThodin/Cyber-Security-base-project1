07b1e5adeed5a88cd130b2017c85ffa49b64d670
VThodin <113366712+VThodin@users.noreply.github.com>
2024-02-07 18:25:07 +0200

    Updated addMemo, flaw 1

--- myMemos/memos/views.py
+++ myMemos/memos/views.py
@@ -48,6 +48,7 @@ def addMemo(request):
     owner_id = request.user.id
     cursor.execute("INSERT INTO memos_memo (memo, owner_id, created_at) VALUES (?,?, ?)", (text, owner_id, datetime.datetime.now()))
     conn.commit()
+    
     return redirect("/")
 
 @login_required
